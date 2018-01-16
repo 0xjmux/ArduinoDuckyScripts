1d88094f82ec3c6decf75147d9acd1af0122252b
Jacob <GarrukApex@users.noreply.github.com>
2018-01-15 18:37:10 -0800

    Uploading Basic Test

---
+++ BasicTest/BasicTest.ino
@@ -0,0 +1,21 @@
+#include <Keyboard.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+pinMode(3, INPUT_PULLUP);     //If switch is pushed, enter programming mode (AKA infinite while loop)
+if (digitalRead(3) == LOW) {
+while(1);
+}
+
+Keyboard.begin();
+delay(1000);
+Keyboard.print("Hello user");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+Hello userHello user
